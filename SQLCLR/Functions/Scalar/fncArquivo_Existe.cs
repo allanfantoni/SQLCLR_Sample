582a616df7a791aad1db9aa7c7a80637572bac95
Allan Fantoni <allan.fantoni@outlook.com>
2024-12-06 16:32:30 -0300

    Add two CLR functions

---
+++ SQLCLR/Functions/Scalar/fncArquivo_Existe.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlTypes;
+using System.IO;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlBoolean fncArquivo_Existe(SqlString Ds_Arquivo)
+    {
+        return File.Exists(Ds_Arquivo.ToString());
+    }
+}
---
+++ SQLCLR/Functions/Scalar/fncCalcula_Idade.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlTypes;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlInt32 fncCalcula_Idade(SqlDateTime Dt_Nascimento, SqlDateTime Dt_Hoje)
+    {
+        if (Dt_Nascimento.IsNull || Dt_Hoje.IsNull)
+            return SqlInt32.Null;
+
+        var anos = Dt_Hoje.Value.Year - Dt_Nascimento.Value.Year;
+
+        if (Dt_Hoje.Value.Month < Dt_Nascimento.Value.Month || (Dt_Hoje.Value.Month == Dt_Nascimento.Value.Month && Dt_Hoje.Value.Day < Dt_Nascimento.Value.Day))
+            anos--;
+
+        return anos;
+    }
+}
